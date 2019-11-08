e10fae5b748709af87a74112abb85437de13067e
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-08 21:44:14 +0530

    Created a Input box and search button and initialized functions

--- byjus/src/App.js
+++ byjus/src/App.js
@@ -17,7 +17,8 @@ export class App extends React.Component{
     </div>
     <div className = "searchText"><h3>Find Your Dream Jobs Here</h3></div>
     <form className = "fields">
-           
+    <input className = "search-Form" type = "text" value = {this.state.value} onChange = {this.handleInput}/>
+           <Button onClick = {this.showResults} variant = "secondary" className = "searchButton">Search</Button>
          </form>
   )
 }
