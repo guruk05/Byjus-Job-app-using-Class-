0e3d5c82deec15e98d109effd33d890f88b3e913
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-10 18:48:52 +0530

    Looping through totalposts/jobsperpage

--- byjus/src/pagination.js
+++ byjus/src/pagination.js
@@ -5,7 +5,11 @@ class Pagination extends React.Component {
     render() {
         const pageNumbers = []; 
 
-       
+        for(let i = 1; i <= Math.ceil(this.props.totalPosts / this.props.jobsPerPage); i++) {
+            pageNumbers.push(i);
+        }
+
+        
 }
 
 export default Pagination;
