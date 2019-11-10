1329e1ed48efc3e35787ff4b7e5b8e44109f544a
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-10 19:54:47 +0530

    returning a header

--- byjus/src/header.js
+++ byjus/src/header.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 
 class Header extends React.Component{
-
+    render() {
+        return(
+            <header className = "header">
+           CracKit
+    </header>
+        )
+    }
+}
 
 export default Header;
