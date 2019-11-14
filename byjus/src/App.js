65b139235a49480583c4bf452e5ba898bdb9df3b
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-14 10:30:23 +0530

    created a variable indexOfFilteredJobs

--- byjus/src/App.js
+++ byjus/src/App.js
@@ -45,6 +45,7 @@ showResults(e) {
   e.preventDefault();
   let filteredJobs = this.state.jobDatas.filter(job => job.title === this.state.value || job.location === this.state.value || job.companyname === this.state.value);
   this.setState({jobDatas:filteredJobs});
+  let indexOfFilteredJobs = filteredJobs.length;
 }
 
 paginate(pageNumber) {
